30f4346ff2fa8337631659674498db80c3061f41
Balajee1403 <balaj7223@gmail.com>
2026-08-19 01:37:59 +0530

    added authentication and search bar

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const User = require("./models/user.js");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
@@ -26,6 +29,22 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: "wanderlust-secret",
+    resave: false,
+    saveUninitialized: false,
+}));
+app.use(async (req, res, next) => {
+
+    if (req.session.userId) {
+        res.locals.currentUser = await User.findById(req.session.userId);
+    } else {
+        res.locals.currentUser = null;
+    }
+
+    next();
+});
+
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
@@ -35,7 +54,7 @@ app.get("/", (req, res) => {
 });
 
 const validateListing = (req, res, next) => {  //func which convert it to middleware
-    let {error} = listingSchema.validate(req.body);
+    let { error } = listingSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -44,9 +63,107 @@ const validateListing = (req, res, next) => {  //func which convert it to middle
     }
 };
 
+// Signup page
+app.get("/signup", (req, res) => {
+    res.render("users/signup.ejs");
+});
+
+// Signup
+app.post("/signup", wrapAsync(async (req, res) => {
+
+    let { username, email, password } = req.body;
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    req.session.userId = newUser._id;
+
+    res.redirect("/listings");
+}));
+
+// Login page
+app.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+// Login
+app.post("/login", wrapAsync(async (req, res) => {
+
+    let { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        return res.send("Invalid email or password");
+    }
+
+    const validPassword = await bcrypt.compare(
+        password,
+        user.password
+    );
+
+    if (!validPassword) {
+        return res.send("Invalid email or password");
+    }
+
+    req.session.userId = user._id;
+
+    res.redirect("/listings");
+}));
+
+app.get("/logout", (req, res) => {
+
+    req.session.destroy((err) => {
+
+        if (err) {
+            return res.send("Unable to logout");
+        }
+
+        res.redirect("/listings");
+    });
+});
+
 //index route
 app.get("/listings", wrapAsync(async (req, res) => {
-    const allListings = await Listing.find({});
+    let { search } = req.query;
+
+    let allListings;
+
+    if (search) {
+        if (!isNaN(search)) {
+
+            let price = Number(search);
+
+            allListings = await Listing.find({
+                price: {
+                    $gte: price - 1000,
+                    $lte: price + 1000
+                }
+            });
+
+        } else {
+
+            allListings = await Listing.find({
+                $or: [
+                    { title: { $regex: search, $options: "i" } },
+                    { description: { $regex: search, $options: "i" } },
+                    { location: { $regex: search, $options: "i" } },
+                    { country: { $regex: search, $options: "i" } }
+                ]
+            });
+
+        }
+    } else {
+        allListings = await Listing.find({});
+    }
+
     res.render("listings/index.ejs", { allListings });
 }));
 
@@ -86,13 +203,13 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route 
-app.put("/listings/:id", 
+app.put("/listings/:id",
     validateListing,
     wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-}));
+        let { id } = req.params;
+        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        res.redirect(`/listings/${id}`);
+    }));
 
 //Delete Route 
 app.delete("/listings/:id", wrapAsync(async (req, res) => {
@@ -125,11 +242,11 @@ app.all("/*splat", (req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    let { statusCode=500, message="Something went wrong!" } = err;
+    let { statusCode = 500, message = "Something went wrong!" } = err;
     res.status(statusCode).render("error.ejs", { message });
     // res.status(statusCode).send(message);
 });
 
- app.listen(8080, () => {
+app.listen(8080, () => {
     console.log("server is running on port 8080");
 });
